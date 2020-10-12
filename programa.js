a7c492b934392308235d4122234f927554c416c4
gvmolin <gvmolin@gmail.com>
2020-10-12 13:17:53 -0300

    calculadora funcionando, faltam implementações

--- programa.js
+++ programa.js
@@ -1,13 +1,44 @@
-var botoesNumericos = document.querySelectorAll("button")
+var botoesNumericos = document.querySelectorAll(".number")
+var botoesOper = document.querySelectorAll(".operador")
+var botaoReset = document.querySelector("#reset")
+var botaoIgualdade = document.querySelector("#igual")
+
+var resultadoCalculadora = document.querySelector(".resultado")
+var contaCalculadora = document.querySelector(".conta")
+
 for (var i = 0; i < botoesNumericos.length; i++){
     var botaoNumerico = botoesNumericos[i]
     botaoNumerico.addEventListener('click', printarBotao)
 }
 
-var resultadoCalculadora = document.querySelector(".resultado")
+for (var i = 0; i < botoesOper.length; i++){
+    var botaoOper = botoesOper[i]
+    botaoOper.addEventListener('click', printarBotao)
+    botaoOper.addEventListener('click', mandarDadosParaTelaCalc)
+}
+
+botaoReset.addEventListener('click', limparTela)
+botaoIgualdade.addEventListener('click', calculo)
+
 function printarBotao(event){
     var botao = event.target
     botao = botao.innerText
     resultadoCalculadora.innerText = resultadoCalculadora.innerText + botao 
+}
+
+function limparTela(){
+    resultadoCalculadora.innerText = ""
+    contaCalculadora.innerText = ""
+}
+
+function mandarDadosParaTelaCalc() {
+    contaCalculadora.innerText = resultadoCalculadora.innerText
+    resultadoCalculadora.innerText = ''
+}
+
+function calculo(){
+    var eq = contaCalculadora.innerText + resultadoCalculadora.innerText
+    resultadoCalculadora.innerText = eval(eq)
+    contaCalculadora.innerText = ""
 }
 
